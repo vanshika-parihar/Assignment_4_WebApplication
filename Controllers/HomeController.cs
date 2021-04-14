56f7a4c66211eaf7aa84f79cf47a3f4778560fbe
Vanshika Kanwar Parihar <vparihar@usf.edu>
2021-04-13 20:54:50 -0400

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace Assignment_4_WebApplication.Controllers
 {
@@ -26,9 +28,50 @@ namespace Assignment_4_WebApplication.Controllers
         {
             return View("Index");
         }
+        /// api call
+        HttpClient httpClient;
+
+        static string BASE_URL = "https://api.usa.gov/crime/fbi/sapi/";
+        static string API_KEY = "g1mjvzQVDzMmHo0UQgfjLMgV4pzyazleokQOM5XS";
+        /*Using general api for explore page*/
         public IActionResult Explore()
         {
-            return View("Explore");
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string FBI_DATA_API_PATH = BASE_URL + "api/participation/national?";
+            string resultsData = "";
+
+            Results results = null;
+
+            httpClient.BaseAddress = new Uri(FBI_DATA_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(FBI_DATA_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    resultsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!resultsData.Equals(""))
+                {
+                    // JsonConvert is part of the NewtonSoft.Json Nuget package
+                    results = JsonConvert.DeserializeObject<Results>(resultsData);
+                }
+            }
+            catch (Exception e)
+            {
+                // This is a useful place to insert a breakpoint and observe the error message
+                Console.WriteLine(e.Message);
+            }
+
+            return View(results);
+            //return View("Explore");
         }
         public IActionResult Report()
         {
@@ -38,10 +81,130 @@ namespace Assignment_4_WebApplication.Controllers
         {
             return View("AboutUs");
         }
+        /*Using Homicide api for HomicideData page*/
+        public IActionResult HomicideData()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string FBI_DATA_API_PATH = BASE_URL + "api/participation/national?";
+            string resultsData = "";
+
+            Results results = null;
+
+            httpClient.BaseAddress = new Uri(FBI_DATA_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(FBI_DATA_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    resultsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!resultsData.Equals(""))
+                {
+     
+                    results = JsonConvert.DeserializeObject<Results>(resultsData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return View(results);
+        }
+        /*Using Property api for PropertyData page*/
+        public IActionResult PropertyData()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string FBI_DATA_API_PATH = BASE_URL + "api/participation/national?";
+            string resultsData = "";
+
+            Results results = null;
+
+            httpClient.BaseAddress = new Uri(FBI_DATA_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(FBI_DATA_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    resultsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!resultsData.Equals(""))
+                {
+
+                    results = JsonConvert.DeserializeObject<Results>(resultsData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return View(results);
+            //return View("page");
+        }
+        /*Using Arrest api for ArrestData page*/
+        public IActionResult ArrestData()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string FBI_DATA_API_PATH = BASE_URL + "api/participation/national?";
+            string resultsData = "";
+
+            Results results = null;
+
+            httpClient.BaseAddress = new Uri(FBI_DATA_API_PATH);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(FBI_DATA_API_PATH).GetAwaiter().GetResult();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    resultsData = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!resultsData.Equals(""))
+                {
+
+                    results = JsonConvert.DeserializeObject<Results>(resultsData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return View(results);
+            //return View("page");
+        }
+
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
---
+++ Models/Results.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Assignment_4_WebApplication.Models
+{
+    public class Results
+    {
+        public Result[] results { get; set; }
+    }
+    public class Result
+    {
+        public int data_year { get; set; }
+        public int population { get; set; }
+        public int total_agency_count { get; set; }
+        public int published_agency_count { get; set; }
+        public int active_agency_count { get; set; }
+        public int covered_agency_count { get; set; }
+        public int population_covered { get; set; }
+        public int agency_count_nibrs_submitting { get; set; }
+        public int agency_count_leoka_submitting { get; set; }//(LEOKA) Law Enforcement Officers Killed and Assaulted 
+        public int agency_count_pe_submitting { get; set; }// pe - police employees
+        public int agency_count_srs_submitting { get; set; }// srs - Summary Reporting System
+        public int agency_count_asr_submitting { get; set; } //asr -AGE, SEX, AND RACE
+        public int agency_count_hc_submitting { get; set; }//hc - Hate Crime
+        public int agency_count_supp_submitting { get; set; }
+        public int nibrs_population_covered { get; set; }
+        public int total_population { get; set; }
+        public float nibrs_population_percentage_covered { get; set; }//nibrs - National Incident-Based Reporting System
+        public string csv_header { get; set; }
+    }
+}
