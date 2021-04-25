8e9554560ae18caf84aa4ce50fee3f9d4b715dd5
shradhapewekar <spewekar@mail.usf.edu>
2021-04-25 18:52:02 -0400

    v0.3

---
+++ Models/Incidents.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_4_WebApplication.Models
+{
+    public class Incidents
+    {
+        public Incident[] incidents { get; set; }
+    }
+    public class Incident
+    {
+
+        public object incident_id { get; set; }
+        public string name { get; set; }
+        public int phone_number { get; set; }
+        public string city { get; set; }
+        public string address { get; set; }
+        public string state { get; set; }
+        public string relation { get; set; }
+        public string description { get; set; }
+
+    }
+}
